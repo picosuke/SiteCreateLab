06c76ffdc267473a2e64d063c1b68f16c599217f
picosuke <picosuke.rice@gmail.com>
2026-04-17 04:43:28 +0900

    Update SCL_renderer.js

--- SCL_renderer.js
+++ SCL_renderer.js
@@ -6,7 +6,6 @@ class SCLConstants extends Blockly.zelos.ConstantProvider {
         this.CUSTOM_TICKET2_RADIUS = 8;
     }
 
-    // 黄色い枠（ハイライト）を出すために、システムの形だけは残しておきます
     shapeFor(connection) {
         const base = super.shapeFor(connection);
         if (!base) return base;
@@ -45,18 +44,16 @@ class TicketDrawer extends Blockly.zelos.Drawer {
         }
     }
 
-    // ★ あなたのアイデアを採用！自分で色を塗る！
     drawInlineInput_(input) {
-        // 黄色い枠を出すための位置計算
         this.positionInlineInputConnection_(input);
 
-        // ブロックのSVG本体に直接アクセス
         let svgRoot = this.block_.getSvgRoot();
         let holeId = 'scl_hole_' + input.name;
         let holePath = svgRoot.querySelector('.' + holeId);
 
-        // ブロックがはまったら、暗い色を隠す
-        if (input.connectedBlock || this.info_.isInserted) {
+        // ★修正2：他のブロックに入った時に消えてしまう原因を削除しました！
+        // 「自分の穴にブロックが入った時」だけ暗い色を消すようにします。
+        if (input.connectedBlock) {
             if (holePath) holePath.style.display = 'none';
             return;
         }
@@ -72,7 +69,6 @@ class TicketDrawer extends Blockly.zelos.Drawer {
             const y = input.centerline - height / 2;
             let path = '';
 
-            // 透明にくり抜くのをやめ、直接シルエットの形を作る
             if (isTicket) {
                 const r = this.constants_.CUSTOM_TICKET_RADIUS;
                 path += `M ${x + r},${y} h ${width - 2 * r} a ${r},${r} 0 0,0 ${r},${r} v ${height - 2 * r} a ${r},${r} 0 0,0 -${r},${r} h -${width - 2 * r} a ${r},${r} 0 0,0 -${r},-${r} v -${height - 2 * r} a ${r},${r} 0 0,0 ${r},-${r} z`;
@@ -80,28 +76,31 @@ class TicketDrawer extends Blockly.zelos.Drawer {
                 const r = this.constants_.CUSTOM_TICKET2_RADIUS;
                 const safeR = Math.min(r, height / 3); 
                 const halfH = height / 2;
-                // ★修正：右側のへこみを直しました！（フラグを 1 にして内側にへこませる）
-                path += `M ${x + width},${y} h -${width} v ${halfH - safeR} a ${safeR},${safeR} 0 0,0 0,${2 * safeR} v ${halfH - safeR} h ${width} v -${halfH - safeR} a ${safeR},${safeR} 0 0,1 0,-${2 * safeR} v -${halfH - safeR} z`;
+                
+                path += `M ${x + width},${y} h -${width} v ${halfH - safeR} `;
+                // ★修正1：左側のフラグを「1」にして、しっかり内側にえぐれるようにしました！
+                path += `a ${safeR},${safeR} 0 0,1 0,${2 * safeR} `; 
+                path += `v ${halfH - safeR} h ${width} v -${halfH - safeR} `;
+                path += `a ${safeR},${safeR} 0 0,1 0,-${2 * safeR} `; 
+                path += `v -${halfH - safeR} z`;
             }
 
-            // 新しいSVG要素を作って、暗い色を「fill」で塗る！
             if (!holePath) {
                 holePath = Blockly.utils.dom.createSvgElement('path', {
                     'class': holeId,
-                    'fill': 'rgba(0, 0, 0, 0.2)' // ★ ここで黒色（透過度20%）を指定！
+                    'fill': 'rgba(0, 0, 0, 0.2)' 
                 }, svgRoot);
             }
             holePath.style.display = 'block';
             holePath.setAttribute('d', path);
 
         } else {
-            // 他の丸い穴などは標準システムに任せる
             super.drawInlineInput_(input);
         }
     }
 }
 
-// ③ 情報処理：形が丸っこくなるのを防ぐ
+// ③ 情報処理
 class TicketRenderInfo extends Blockly.zelos.RenderInfo {
     makeInput_(input) {
         const res = super.makeInput_(input);
