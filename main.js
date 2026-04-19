cc56fe05e025d39fe4e6a5cee0aaf8ebee924163
picosuke <picosuke.rice@gmail.com>
2026-04-19 11:57:26 +0900

    Update and rename SiteCreateLab.js to main.js

---
+++ main.js
@@ -0,0 +1,65 @@
+var title = "";
+var text = "";
+var csstext = "";
+var jstext = "";
+var cssd = "";
+var YOMI = "";
+var classd = "";
+var cssk = ""; // 追加
+
+function callbackFn ( event ) {
+	event.returnValue = "入力内容が残ってますが、このページを離れても大丈夫ですか？" ;
+}
+window.onbeforeunload = callbackFn;
+
+Blockly.Themes.dark = Blockly.Theme.defineTheme('dark', {
+    'base': Blockly.Themes.Classic,
+    'componentStyles': {
+        'toolboxBackgroundColour': '#ddd',
+        'toolboxForegroundColour': '#fff',
+        'flyoutBackgroundColour': '#8aa',
+        'flyoutForegroundColour': '#ccc',
+        'insertionMarkerColour': '#fff',
+        'scrollbarOpacity': 0.7,
+    }
+});
+
+var workspace = Blockly.inject(`blocklyDiv`, {
+    toolbox: document.getElementById(`toolbox`),
+    theme: Blockly.Themes.dark,
+    renderer: 'SCL_renderer',
+    sounds : false, 
+    media: './media/',
+    move: {
+        wheel: true,
+    },
+    zoom: {
+        controls: true,
+        wheel: true,
+        startScale: 1.0,
+        maxScale: 3,
+        minScale: 0.3,
+        scaleSpeed: 1.2,
+        pinch: true
+    },
+    grid: {
+        spacing: 23,
+        length: 3,
+        colour: '#ccc',
+    },
+    trashcan: true,
+    scrollbars: true,
+    plugins : { 
+        blockDragger : ScrollBlockDragger, 
+        metricsManager : ScrollMetricsManager,
+    }, 
+});
+
+Blockly.ContextMenuRegistry.registry.unregister('blockCollapseExpand');
+Blockly.ContextMenuRegistry.registry.unregister('blockInline');
+Blockly.ContextMenuRegistry.registry.unregister('blockDisable');
+Blockly.ContextMenuRegistry.registry.unregister('blockComment');
+
+Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'),workspace);
+
+workspace.addChangeListener(Blockly.Events.disableOrphans);
